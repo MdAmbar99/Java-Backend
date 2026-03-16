2b503dabbb46c5f91e8533fee4fe8f637e81bc53
Md Ambar Shafi <shafiambar3@gmail.com>
2026-03-17 00:42:55 +0530

    User Input

---
+++ CORE_JAVA/UserInput.java
@@ -0,0 +1,29 @@
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.BufferedReader;
+
+public class UserInput{
+    public static void main(String args[]) throws IOException{
+        System.out.println("Enter a number : ");
+
+        // int num = System.in.read();
+
+        // InputStreamReader in = new InputStreamReader(System.in);
+        // BufferedReader bf = new BufferedReader(in);
+
+        // int num = Integer.parseInt(bf.readLine());
+
+        // System.out.println(num);
+
+        // bf.close();
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        System.out.println(num);
+    }
+}
